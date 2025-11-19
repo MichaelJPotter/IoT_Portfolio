7af66e5788e942536684a147749a085d7bda423a
conmorgs <CONNOR.MORGAN23@bathspa.ac.uk>
2025-11-19 14:23:55 +0000

    Adding Comments
    Adding descriptive comments to code

--- TravelTensorFlow.py
+++ TravelTensorFlow.py
@@ -1,14 +1,22 @@
 #import necessary Libraries 
+#------------------------
 #Pandas for handling CSV data 
 import pandas as pd
+#numpy for xxx (Fill this)
 import numpy as np
+# for xxx (Fill this)
 import glob
+# for xxx (Fill this)
 import os
+#matplot lib for plotting 
 import matplotlib.pyplot as plt
+#sklearn for data processing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, classification_report
+#seaborn for data visualisation
 import seaborn as sns
+#tensorflow for deeplearning
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
