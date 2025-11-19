7c45f97d762dd72506d5b46fec09e0b73685fec8
MichaelJPotter <michael.potter23@bathspa.ac.uk>
2025-11-19 14:11:27 +0000

    Imported necessary libraries for plotting of graphs and data, some may be added and removed as needed

--- TravelTensorFlow.py
+++ TravelTensorFlow.py
@@ -1,3 +1,13 @@
+#import necessary Libraries 
 import pandas as pd
 import numpy as np
-import tensor as tf
+import glob
+import os
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
